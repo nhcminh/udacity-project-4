09de1d69cae470a648a611c27202f399ae72ad6a
nhcminh <a2mrken@gmail.com>
2022-10-09 22:02:17 +0700

    Complete Task 4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=minhnhc/project-api
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u minhnhc
+docker tag project-api $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
+
+### DOCKER REPO ###
+# https://hub.docker.com/repository/docker/minhnhc/project-api
