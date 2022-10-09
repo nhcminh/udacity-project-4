00c8fca5daffedbe1d5ac4324c1fdc4dc723d736
nhcminh <a2mrken@gmail.com>
2022-10-09 21:04:56 +0700

    Complete Task 2

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,8 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=project-api .
+docker build . --tag=project-api
+echo "Build Complete"
 # Step 2: 
 # List docker images
 docker image ls
