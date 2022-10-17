45d33cf7ed47a9fd1378b6f57d8b8744b58a7cdb
nhcminh <a2mrken@gmail.com>
2022-10-17 11:12:15 +0700

    Complete Task 6

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=minhnhc/api
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run app --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward app 8080:80
